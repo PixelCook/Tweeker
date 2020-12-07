a31a84b1237ed3930e88bd38d3f0641607c11309
User <user@Users-MacBook-Pro.local>
2020-12-07 13:42:53 +0200

    intial

--- firebase.js
+++ firebase.js
@@ -12,15 +12,4 @@ var db = firebase.firestore();
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-db.collection("users")
-  .add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815,
-  })
-  .then(function (docRef) {
-    console.log("Document written with ID: ", docRef.id);
-  })
-  .catch(function (error) {
-    console.error("Error adding document: ", error);
-  });
+<script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-auth.js"></script>;
